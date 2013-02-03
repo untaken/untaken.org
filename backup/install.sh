0b5e7c1bd216e5553a63adaca7eebcfcca8aea22
Luke <luke@luke-home.(none)>
2013-02-03 12:49:21 +0000

    a start on install of apps

---
+++ backup/install.sh
@@ -0,0 +1,5 @@
+sudo apt-get -y install aptitude
+sudo aptitude -y install xmonad vim-gtk vim git subversion rxvt-unicode-256color xmobar xscreensaver orage libplack-perl xcompmgr pidgin trayer tmux feh autokey-gtk
+#git clone https://github.com/untaken/untaken.org.git ~/untaken
+ln -s ~/untaken/backup/.xsession ~/.xsession
+ln -s ~/untaken/backup/.Xdefaults ~/.Xdefaults
