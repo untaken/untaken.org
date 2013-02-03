9c78d4d933383ede2d8d8fd54ca4556c9ae0f294
Luke Harwood <luke.s.harwood@gmail.com>
2013-02-03 13:08:37 +0000

    more additions

--- backup/install.sh
+++ backup/install.sh
@@ -1,5 +1,9 @@
 sudo apt-get -y install aptitude
-sudo aptitude -y install xmonad vim-gtk vim git subversion rxvt-unicode-256color xmobar xscreensaver orage libplack-perl xcompmgr pidgin trayer tmux feh autokey-gtk
-#git clone https://github.com/untaken/untaken.org.git ~/untaken
+sudo aptitude -y install xmonad vim-gtk vim git subversion rxvt-unicode-256color xmobar xscreensaver orage libplack-perl xcompmgr pidgin trayer tmux feh autokey-gtk suckless-tools
+git clone https://github.com/untaken/untaken.org.git ~/untaken
+mkdir ~/.xmonad/ ~/bin/
 ln -s ~/untaken/backup/.xsession ~/.xsession
 ln -s ~/untaken/backup/.Xdefaults ~/.Xdefaults
+ln -s ~/untaken/backup/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s ~/untaken/backup/notifty-service.psgi ~/bin/notifty-service.psgi
+cp ~/untaken/backup/default.desktop /usr/share/xsessions/default.desktop
